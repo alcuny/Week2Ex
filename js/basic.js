614fa4ff4e45b1d57ac00f0128bbe50a0686a9f4
Albert Cuny01~ <cunyalbert16@gmail.com01~>
2023-09-07 18:29:28 +0300

    ex 5 done!

--- js/basic.js
+++ js/basic.js
@@ -4,49 +4,75 @@ const myTable = document.getElementsByTagName('table')[0];
 const empty = document.getElementById('empty-table');
 
 myForm.addEventListener('submit', (event) => {
-  // if not for this the data would disapear  
-  event.preventDefault();
+    event.preventDefault();
+    
+    const username = document.getElementById('input-username').value;
+    const email = document.getElementById('input-email').value;
+    const admin = document.getElementById('input-admin').checked ? 'X' : '-';
+    const pictureInput = document.getElementById('input-image');
+    
+    // Check if a file is selected
+    if (pictureInput.files.length > 0) {
+      const picture = pictureInput.files[0];
+      
+      // check if the username exist by calling duplicatechecker function
+      if (!duplicateChecker(username, email, admin, picture)) {
+        const newRow = document.createElement('tr');
+
+        const usernameCell = document.createElement('td');
+        usernameCell.textContent = username;
+        newRow.appendChild(usernameCell);
+
+        const emailCell = document.createElement('td');
+        emailCell.textContent = email;
+        newRow.appendChild(emailCell);
+
+        const adminCell = document.createElement('td');
+        adminCell.textContent = admin;
+        newRow.appendChild(adminCell);
+
+        const img = document.createElement('img');
+        img.src = URL.createObjectURL(picture);
+        img.width = 64;
+        img.height = 64;
+        const imageCell = document.createElement('td'); // Create a new cell for the image
+        imageCell.appendChild(img);
+        newRow.appendChild(imageCell);
+
+        myTable.querySelector('tbody').appendChild(newRow);
+      }
+    }
+  });
   
-  const username = document.getElementById('input-username').value;
-  const email = document.getElementById('input-email').value;
-  // cheks if the ckeckbox is true or false(checked or unchecked)
-  const admin = document.getElementById('input-admin').checked ? 'X' : '-';
-  // adding a new row only if the username doesn't already exist
-  if(duplicateChecker(username, email, admin) == false){
-  
-  // building a row scruture. Buildingblocks: td tags and a tr tag
-  const newRow = document.createElement('tr');
-  const usernameCell = document.createElement('td');
-  usernameCell.textContent = username;
-  newRow.appendChild(usernameCell);
-  const emailCell = document.createElement('td');
-  emailCell.textContent = email;
-  newRow.appendChild(emailCell);
-  const adminCell = document.createElement('td');
-  adminCell.textContent = admin;
-  newRow.appendChild(adminCell);
-  myTable.querySelector('tbody').appendChild(newRow);
-  
-  }
-});
 
 // When a duplicate is found edits the existing cell, is called when submiting form
-function duplicateChecker(username, email, admin) {
-    
+function duplicateChecker(username, email, admin, picture) {
     let check = false;
-
     const rows = myTable.querySelectorAll('tbody tr');
-    //console.log(rows[0].childNodes[0].textContent);
+    
     for (let row = 0; row < rows.length; row++) { 
-        
         if(username === rows[row].childNodes[0].textContent){
             rows[row].childNodes[1].textContent = email;
             rows[row].childNodes[2].textContent = admin;
-            check = true
+            
+            let img = rows[row].childNodes[3].querySelector('img');
+            
+            // If the img element doesn't exist in the fourth column, create it
+            if (!img) {
+                img = document.createElement('img');
+                img.width = 100;
+                img.height = 100;
+                rows[row].childNodes[3].appendChild(img);
+            }
+            
+            img.src = URL.createObjectURL(picture);
+            
+            check = true;
         }    
     }
-    return check
+    return check;
 }
+
 
 empty.addEventListener('click', function() {
     // selecting all the rows from the body
